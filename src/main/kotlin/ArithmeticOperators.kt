e92f60d76355699f4ea570ca79c6fe99046ef5cc
James Nyamu <nyamuj59@gmail.com>
2022-03-09 22:26:44 +0300

    Introduction to kotlin arithmetic operations

---
+++ src/main/kotlin/ArithmeticOperators.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var x = 12
+    var y = 5
+
+    println("a=" + x + " b=" + y)
+
+    println("the sum is ${x + y}")
+    println("the product is ${x * y}")
+    println("the difference is ${x - y}")
+    println("the division is ${x / y}")
+    println("the remainder is ${x % y}")
+}
+
