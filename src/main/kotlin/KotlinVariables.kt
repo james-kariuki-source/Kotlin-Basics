faf9c4b380b887fa2c6cb2f29ec7f5fa606e8489
James Nyamu <nyamuj59@gmail.com>
2022-03-09 11:16:03 +0300

    Introduction to kotlin variables.

---
+++ src/main/kotlin/KotlinVariables.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val userName = "James"
+    var userCity: String = "Nairobi"
+    var age: Int = 20
+
+    println("My name is $userName. I am $age years old. In 2 years I'll be ${age + 3} years old.")
+    print("I am from $userCity")
+}
