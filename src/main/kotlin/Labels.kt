148eb2b9039fe6eecc4090b88c19f5bc7829a887
James Nyamu <nyamuj59@gmail.com>
2022-03-12 18:20:18 +0300

    Added the labels file

---
+++ src/main/kotlin/Labels.kt
@@ -0,0 +1,18 @@
+fun main(){
+    var g = 1
+
+  outer@  do{
+        println(g)
+        g++
+        //continue
+
+        var l = 0
+        while(l <= 5){
+            println("----$l")
+            l++
+            break@outer
+            println("This should not be printed.")
+        }
+        println("This should be printed.")
+    } while(g <= 5)
+}
