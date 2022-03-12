53f848322f1ac5d74bbc27cc346d373eda8f5565
James Nyamu <nyamuj59@gmail.com>
2022-03-12 18:05:18 +0300

    Nested loops.

---
+++ src/main/kotlin/NestedLoop.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var g = 1
+
+    do{
+        println(g)
+        g++
+
+        var l = 0
+        while(l <= 5){
+          println("----$l")
+          l++
+        }
+
+    } while(g <= 5)
+}
