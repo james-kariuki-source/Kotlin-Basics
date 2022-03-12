93b00eb7935999c44f86d551121e9f3d36c7c1e4
James Nyamu <nyamuj59@gmail.com>
2022-03-12 15:47:20 +0300

    Some changes to the file names

---
+++ src/main/kotlin/ForLoops.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val animals = arrayOf("Dog", "Tiger", "Panther", "Elephant", "Hare")
+
+    for (animal in animals){
+        println(animal)
+    }
+
+    for (animal in animals)
+        println(animal)
+
+    for (animal in animals) println(animal)
+//The above is personal preference
+
+}
