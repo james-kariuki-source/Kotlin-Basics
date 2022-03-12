e6da82902b9dc7bab32244bf9b640907fe90ed6f
James Nyamu <nyamuj59@gmail.com>
2022-03-12 17:50:05 +0300

    Introduction to comparison & logical operators and while loops.

---
+++ src/main/kotlin/Comparison.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("5 > 1 is ${5 > 1}")
+    println("2 < 5 is ${2 < 5}")
+    println("2 >= 2 is ${2 >= 2}")
+    println("6 != 6 is ${6 != 6}")
+    println("10 == 3 is ${5 == 6}")
+
+    println("True && False = ${true && false} ")
+    println("True || False = ${true || false} ")
+
+}
---
+++ src/main/kotlin/WhileLoops.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var g = 1
+
+    while (g <= 10){
+        println(g)
+        g += 2
+    }
+}
