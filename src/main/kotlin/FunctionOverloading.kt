031c202c042eb86e195d5caf8b2c93288f77f9e8
James Nyamu <nyamuj59@gmail.com>
2022-03-26 14:30:27 +0300

    Functions and function overloading

---
+++ src/main/kotlin/FunctionOverloading.kt
@@ -0,0 +1,5 @@
+fun main(){
+    println(max(14, 6))
+}
+
+fun max(x: Int, y:Int) = if(x > y) x else y
--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,7 +1,21 @@
 fun main(){
+    val max = getMax(12, 6)
     hello("Elanga")
+    println(max)
+    println(getDouble(12))
 }
 
 fun hello(name: String){
     println("Hello $name, Welcome to this kotlin file.")
 }
+
+fun getMax(num1: Int, num2: Int): Int{
+    return if(num1 > num2){
+        num1
+    } else{
+        num2
+    }
+}
+
+//Single expression function
+fun getDouble(multi: Int) = multi*2
